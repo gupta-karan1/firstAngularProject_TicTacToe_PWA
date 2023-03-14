1290cfd8c7940ba3700a6cedfc4f99d9aa12a2f8
Karan Gupta <d22124440@mytudublin.ie>
2023-03-14 18:58:49 +0000

    wip

---
+++ ng-video-game-db/src/app/interceptors/http-errors.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((err) => {
+        console.log(err);
+        return observableThrowError(err);
+      })
+    );
+  }
+}
+function observableThrowError(err: any): any {
+  throw new Error('Function not implemented.');
+}
